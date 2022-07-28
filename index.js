bb6bc4f4c89f8bce509d5cf3d204aaa4ceb03a47
Paola Santillan <a.paolasantillangzz@hotmail.com>
2022-07-27 19:04:23 -0500

    Proyecto API

--- index.js
+++ index.js
@@ -1,148 +1,79 @@
-/*const input = document.getElementById('buscador').value;
-const button = document.getElementById('buscarBtn');
-const pokemonContainer = document.getElementById('app');*/
-
-/*function getPokemon(){
-    let url = "https://pokeapi.co/api/v2/pokemon/"
-    let pokemones = []
-
-    fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        console.log('respuesta server', data)
-        pokemones = data;
-    })
-
-}
-
-getPokemon();*/
-
-/*function getPokemon(id){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    .then(res => res.json())
-    .then(data => {
-        createPokemon(data);
-    });
-}
-
-getPokemon(1);
-
-
-function fetchPokemon(number){
-    for(let i = 1; i <= number; i++) {
-        fetchPokemon(i);
-    }
-}
-
-function createPokemon(pokemon){
-    const card = document.createElement('div');
-    card.classList.add('pokemon-block');
-
-    const spriteContainer = document.createElement('div');
-    spriteContainer.classList.add('img-container');
-
-    const sprite = document.createElement('img');
-    sprite.src = pokemon.sprites.front_default
-
-    spriteContainer.appendChild(sprite);
-
-    const number = document.createElement('p');
-    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
-
-    const name = document.createElement('p');
-    name.classList.add('name');
-    name.textContent = pokemon.name
-
-    card.appendChild(spriteContainer);
-    card.appendChild(number);
-    card.Container.appendChild(card);
-}
-
-fetchPokemon(9)*/
-
-
 class Poke {
-    constructor(name, image) {
+    constructor(name, url) {
         this.name = name;
-        this.image = image;
+        this.url = url;
     }
 
     obtenerNombre() {
         return this.name;
     }
 
-    obtenerFoto() {
-        return this.image;
+    async obtenerFoto() {
+        //console.log(this.url)
+        let resultado = await fetch(this.url)
+        let data1 = await resultado.json();
+        //console.log(data1)
+        //console.log(resultado)
+        console.log(data1.sprites.front_default)
+        return data1.sprites.front_default;
     }
 }
 
-async function getPokemon(id) {
-    let poke = [];
-    let url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
-    let elemento = document.getElementById("app");
+let varPoke = [];
+let elemento = document.getElementById("app");
+
+async function getPokemon() {
+    let url = `https://pokeapi.co/api/v2/pokemon/`;
+    
     
     const data = await fetch(url);
-    const pokemones = await data.json();
+    let pokemones = await data.json();
+
 
     console.log(pokemones);
 
-    poke = new Poke(pokemones.name, pokemones.sprites.front_default)
+
+    pokemones.results.map(async datum => {
+        const personaje = new Poke(datum.name, datum.url)
+        const foto = await personaje.obtenerFoto()
+        //personaje.obtenerFoto();
+        console.log(foto)
+        elemento.innerHTML += 
+        `<div class="pokeapi">
+        <h2 class="colorletter">${datum.name}</h2>
+        <img src="${foto}">
+        </div>`
+
+            
+
+    })
+
+    const input = document.querySelector('#buscador')
+
+    document.getElementById('app').innerHTML = pok;
+    }
 
 
-    elemento.innerHTML = `
-    <p>${pokemones.name}</p>
-    <img src=${pokemones.sprites.front_default} height="250" width="250"/>`
+    async function llamarBusqueda() {
+        const consulta = document.getElementById("buscador").value;
+        let url = `https://pokeapi.co/api/v2/pokemon/${consulta}`;
+        const data = await fetch(url);
+        let pokemones = await data.json();
 
-}
+        elemento.innerHTML = `<div class="pokeapi">
+        <h2 class="colorletter">${pokemones.name}</h2>
+        <img src="${pokemones.sprites.front_default}">
+        </div>`
 
-getPokemon(5);
+        console.log(data);
 
-/*async function createPokemon(id) {
-    let url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
-    let elemento = document.getElementById("app");
-    const data = await fetch(url);
-    const pokemones = await data.json();
-
-    console.log(pokemones);
-
-    elemento.innerHTML = `<img src=${pokemon[1].sprites.front_default}/>`
-}*/
-
-
-
-
-
-
-/*function createPokemon(pokemon) {
-    const img = document.createElement('img');
-    img.src = pokemon.sprites.front_default;
-
-    const h3 = document.createElement('h3');
-    h3.textContent = pokemon.name;
-
-    const div = document.createElement('div');
-    div.appendChild(img);
-    div.appendChild(h3);
-
-    pokemonContainer.appendChild(div);
-
-}
-
-getPokemon();*/
+    }
 
 
 
 
-/*async function getPokemon() {
-    let url = `https://pokeapi.co/api/v2/pokemon/`;
-    let elemento = document.getElementById("app");
+    getPokemon();
 
-    const data = await fetch(url);
-    let pokemon = await data.json();
+    
 
-    console.log(pokemon.url);
-
-    elemento.innerHTML = `<img src=${pokemon.url}/>`;
-}
-
-getPokemon();*/
+    
